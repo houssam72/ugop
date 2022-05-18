b7343742db992224c13e1267df1e2cb555ebd89b
houssam72 <brothersmarket72@gmail.com>
2022-05-18 13:20:33 +0300

    Updates

---
+++ precache-manifest.61b980ae8c04c42dbef6300a0016f69e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91fe645626cedefb38fda4c07158557d",
+    "url": "/ugop/index.html"
+  },
+  {
+    "revision": "87b4ddbb4f6a6841de92",
+    "url": "/ugop/static/css/2.3b2cfc2e.chunk.css"
+  },
+  {
+    "revision": "1790565b82dc137df312",
+    "url": "/ugop/static/css/main.ac560fc8.chunk.css"
+  },
+  {
+    "revision": "87b4ddbb4f6a6841de92",
+    "url": "/ugop/static/js/2.d69d271c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/ugop/static/js/2.d69d271c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1790565b82dc137df312",
+    "url": "/ugop/static/js/main.f0a224a6.chunk.js"
+  },
+  {
+    "revision": "16f257458a85797f4755",
+    "url": "/ugop/static/js/runtime-main.09c0e5e9.js"
+  }
+]);
